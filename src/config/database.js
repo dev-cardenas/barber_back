36f083828278368f63cf0c59055b67ddd717d94c
dev-cardenas <dev.cardenas@gmail.com>
2019-12-27 21:23:09 -0300

    change port in app db

--- src/config/database.js
+++ src/config/database.js
@@ -6,6 +6,7 @@ module.exports = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
+  port: process.env.POSTGRES_PORT,
   define: {
     timestamps: true,
     underscored: true,

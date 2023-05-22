e535ab2855b9475442c25b18b1c9b5858303729d
dev-cardenas <dev.cardenas@gmail.com>
2023-05-22 10:26:28 -0400

    prismacollection improvements

---
+++ src/useCases/ruc/constants/transactionType.ts
@@ -0,0 +1,32 @@
+export const transactionType = [
+  {
+    code: 1,
+    description: 'Venta de mercadería',
+    situation: 0,
+  },
+  {
+    code: 2,
+    description: 'Presentación de servicios',
+    situation: 1,
+  },
+  {
+    code: 3,
+    description: 'Mixto (Venta de mercadería y servicios)',
+    situation: 1,
+  },
+  {
+    code: 4,
+    description: 'Venta activo fijo',
+    situation: 0,
+  },
+  {
+    code: 5,
+    description: 'Venta de divisas',
+    situation: 0,
+  },
+  {
+    code: 6,
+    description: 'Venta de mercadería',
+    situation: 0,
+  },
+]

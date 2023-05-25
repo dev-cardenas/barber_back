205c2e26dc25121f00ac66daa6a17bf805e7b0a3
dev-cardenas <dev.cardenas@gmail.com>
2023-05-25 09:06:32 -0400

    fix typeuser

--- src/useCases/TypeUser/routes.ts
+++ src/useCases/TypeUser/routes.ts
@@ -11,9 +11,9 @@ import {
 export const api = '/api/v1'
 
 export async function rolesRoutes(app: FastifyInstance) {
-  app.post(`${api}/TypeUser/create`, CreateTypeUserController)
-  app.get(`${api}/TypeUser/:id_user_role`, GetByIdTypeUserController)
-  app.get(`${api}/TypeUser`, GetAllTypeUserController)
-  app.put(`${api}/TypeUser/update`, UpdateTypeUserController)
-  app.delete(`${api}/TypeUser/:id_user_role`, DeleteByIdTypeUserController)
+  app.post(`${api}/typeUser/create`, CreateTypeUserController)
+  app.get(`${api}/typeUser/:id_user_role`, GetByIdTypeUserController)
+  app.get(`${api}/typeUser`, GetAllTypeUserController)
+  app.put(`${api}/typeUser/update`, UpdateTypeUserController)
+  app.delete(`${api}/typeUser/:id_user_role`, DeleteByIdTypeUserController)
 }
--- src/useCases/typeProcessAppointments/routes.ts
+++ src/useCases/typeProcessAppointments/routes.ts
@@ -11,9 +11,24 @@ import {
 export const api = '/api/v1'
 
 export async function rolesRoutes(app: FastifyInstance) {
-  app.post(`${api}/typeProcessAppointments/create`, CreateTypeProcessAppointmentsController)
-  app.get(`${api}/typeProcessAppointments/:id_type_process_appointments`, GetByIdTypeProcessAppointmentsController)
-  app.get(`${api}/typeProcessAppointments`, GetAllTypeProcessAppointmentsController)
-  app.put(`${api}/typeProcessAppointments/update`, UpdateTypeProcessAppointmentsController)
-  app.delete(`${api}/typeProcessAppointments/:id_type_process_appointments`, DeleteByIdTypeProcessAppointmentsController)
+  app.post(
+    `${api}/typeProcessAppointments/create`,
+    CreateTypeProcessAppointmentsController,
+  )
+  app.get(
+    `${api}/typeProcessAppointments/:id_type_process_appointments`,
+    GetByIdTypeProcessAppointmentsController,
+  )
+  app.get(
+    `${api}/typeProcessAppointments`,
+    GetAllTypeProcessAppointmentsController,
+  )
+  app.put(
+    `${api}/typeProcessAppointments/update`,
+    UpdateTypeProcessAppointmentsController,
+  )
+  app.delete(
+    `${api}/typeProcessAppointments/:id_type_process_appointments`,
+    DeleteByIdTypeProcessAppointmentsController,
+  )
 }
